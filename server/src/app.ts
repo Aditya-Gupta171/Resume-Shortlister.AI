32bfc0c147a1f654b7b1f8a2cedeb37403379c6d
Aditya-Gupta171 <adityagupta14april@gmail.com>
2026-05-18 01:05:51 +0530

    add Dockerfile and .dockerignore; update CORS origin in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ app.use("*", logger());
 app.use(
   "*",
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173"],
     allowMethods: ["GET", "POST"],
     allowHeaders: ["Content-Type"],
   })
